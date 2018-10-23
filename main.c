13d0148a49530432865a82d89f9525df66c849d0
Alexander Wranovsky <alex@wranovsky.org>
2018-10-23 17:35:10 -0500

    Added IP stack initialization to main, added file to put our firmware update code in.

--- main.c
+++ main.c
@@ -14,12 +14,17 @@
 // FreeRTOS includes
 #include "FreeRTOS.h"
 #include "task.h"
+#include "FreeRTOS_IP.h"
+#include "FreeRTOS_IP_Private.h"
 
 
 // FreeRTOS system interrupt handlers
 extern void vPortSVCHandler(void);
 extern void xPortPendSVHandler(void);
 extern void xPortSysTickHandler(void);
+
+// needed to read mac address (definition is in ethernet.c)
+extern bool eth_read_mac_addr( uint8_t *mac_buf );
 
 // place all your declarations here
 static void example_freertos_task(void *params);
@@ -44,6 +49,21 @@ int main(){
     IntRegister(FAULT_PENDSV, xPortPendSVHandler);
     // Register FreeRTOS's SysTick handler
     IntRegister(FAULT_SYSTICK, xPortSysTickHandler);
+
+    {
+        static const uint8_t Ip_Address[ipIP_ADDRESS_LENGTH_BYTES]      = {192,168,1,10};
+        static const uint8_t Net_Mask[ipIP_ADDRESS_LENGTH_BYTES]        = {255,255,255,0};
+        static const uint8_t Gateway_Address[ipIP_ADDRESS_LENGTH_BYTES] = {192,168,1,1};
+        static const uint8_t DNS_Address[ipIP_ADDRESS_LENGTH_BYTES]     = {8,8,8,8};
+        static       uint8_t Mac_Address[6];
+        eth_read_mac_addr(Mac_Address);
+        FreeRTOS_IPInit(
+            Ip_Address,
+            Net_Mask,
+            Gateway_Address,
+            DNS_Address,
+            Mac_Address);
+    }
 
     // startup FreeRTOS, this function will never return on a system that's operating correctly
     vTaskStartScheduler();
---
+++ network_firmware_update.h
@@ -0,0 +1,8 @@
+#ifndef NETWORK_FIRMWARE_UPDATE_H_
+#define NETWORK_FIRMWARE_UPDATE_H_
+
+/*
+ * Put header things here
+ */
+
+#endif
