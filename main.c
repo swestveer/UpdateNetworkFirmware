b341ac48313d7787f0330340c4c4c9ebca1d40a2
Avichal Rakesh <arakesh@wisc.edu>
2018-10-30 16:55:26 -0500

    move init code to main - fix tcp

--- main.c
+++ main.c
@@ -30,10 +30,27 @@ extern bool eth_read_mac_addr( uint8_t *mac_buf );
 
 // place all your declarations here
 static void example_freertos_task(void *params);
-void init_controller(void);
 
 int main() {
-    init_controller();
+    //////// NOTE: Do not move this section from main(). It breaks TCP ////////
+    // set the system clock to the maximum value
+    uint32_t sys_clock = SysCtlClockFreqSet(
+            SYSCTL_XTAL_25MHZ|SYSCTL_OSC_MAIN|SYSCTL_USE_PLL|SYSCTL_CFG_VCO_480,
+            120000000
+            );
+
+   //Wait for external peripherals to initialize
+   {
+        uint32_t i = 0;
+        while (i++ < 1000000);
+   }
+   // Register FreeRTOS's Supervisor Call handler
+   IntRegister(FAULT_SVCALL, vPortSVCHandler);
+   // Register FreeRTOS's Pend Supervisor Call handler
+   IntRegister(FAULT_PENDSV, xPortPendSVHandler);
+   // Register FreeRTOS's SysTick handler
+   IntRegister(FAULT_SYSTICK, xPortSysTickHandler);
+
     {
         static const uint8_t Ip_Address[ipIP_ADDRESS_LENGTH_BYTES]      = {192,168,1,10};
         static const uint8_t Net_Mask[ipIP_ADDRESS_LENGTH_BYTES]        = {255,255,255,0};
@@ -48,35 +65,13 @@ int main() {
             DNS_Address,
             Mac_Address);
     }
+    //////// End Section ////////
 
     // startup FreeRTOS, this function will never return on a system that's operating correctly
     vTaskStartScheduler();
 
     return 0;
 }
-
-void init_controller(void) {
-    // set the system clock to the maximum value
-       uint32_t sys_clock = SysCtlClockFreqSet(
-               SYSCTL_XTAL_25MHZ|SYSCTL_OSC_MAIN|SYSCTL_USE_PLL|SYSCTL_CFG_VCO_480,
-               120000000
-       );
-
-       //Wait for external peripherals to initialize
-       {
-           uint32_t i = 0;
-           while (i++ < 1000000);
-       }
-
-       // Register FreeRTOS's Supervisor Call handler
-       IntRegister(FAULT_SVCALL, vPortSVCHandler);
-       // Register FreeRTOS's Pend Supervisor Call handler
-       IntRegister(FAULT_PENDSV, xPortPendSVHandler);
-       // Register FreeRTOS's SysTick handler
-       IntRegister(FAULT_SYSTICK, xPortSysTickHandler);
-}
-
-
 
 static void example_freertos_task(void *params) {
     // A FreeRTOS task should never return. If a task needs to be killed, use vTaskDelete().
