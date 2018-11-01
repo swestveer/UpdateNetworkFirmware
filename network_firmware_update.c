2935a1ce1677bc8e9d44c84dbdbe79e52a703b62
Avichal Rakesh <arakesh@wisc.edu>
2018-11-01 17:41:26 -0500

    try more echoing

--- network_firmware_update.c
+++ network_firmware_update.c
@@ -6,7 +6,7 @@ void echo (Socket_t);
 
 uint8_t rip_init(TaskHandle_t *task_handle) {
     BaseType_t create_return = NULL;
-    create_return = xTaskCreate(rip_start_firmware, "RIP", configMINIMAL_STACK_SIZE, NULL, configMAX_PRIORITIES - 1, task_handle);
+    create_return = xTaskCreate(rip_start_firmware, "RIP", configMINIMAL_STACK_SIZE, NULL, 5, task_handle);
     if(create_return ==  pdPASS) {
         return 0;
     }
@@ -14,15 +14,20 @@ uint8_t rip_init(TaskHandle_t *task_handle) {
 }
 
 void rip_start_firmware(void *args) {
+    struct freertos_sockaddr bindAddr;
     Socket_t connectedSocket;
+    static const TickType_t receiveTimeOut = portMAX_DELAY;
+
     Socket_t listeningSocket = FreeRTOS_socket (FREERTOS_AF_INET, FREERTOS_SOCK_STREAM, FREERTOS_IPPROTO_TCP);
     configASSERT(listeningSocket != FREERTOS_INVALID_SOCKET);
 
-    struct freertos_sockaddr bindAddr;
-    bindAddr.sin_port = (uint16_t) 8080;
+    FreeRTOS_setsockopt(listeningSocket, 0, FREERTOS_SO_RCVTIMEO, &receiveTimeOut, sizeof(receiveTimeOut));
+
+    bindAddr.sin_port = (uint16_t) 8000;
     bindAddr.sin_port = FreeRTOS_htons(bindAddr.sin_port);
 
     FreeRTOS_bind (listeningSocket, &bindAddr, sizeof(bindAddr));
+    FreeRTOS_listen(listeningSocket, 1);
 
     struct freertos_sockaddr client;
     socklen_t clientSize = sizeof(client);
@@ -30,8 +35,9 @@ void rip_start_firmware(void *args) {
         connectedSocket = FreeRTOS_accept(listeningSocket, &client, &clientSize);
         configASSERT(connectedSocket != FREERTOS_INVALID_SOCKET);
         // Work on the socket!
-
-        echo(connectedSocket);
+        if (connectedSocket) {
+            echo(connectedSocket);
+        }
     }
 }
 
@@ -46,7 +52,7 @@ void echo (Socket_t socket) {
 
     if (buf != NULL) {
         FreeRTOS_setsockopt(socket, 0, FREERTOS_SO_RCVTIMEO, &receiveTimeOut, sizeof( receiveTimeOut ));
-        FreeRTOS_setsockopt( socket, 0, FREERTOS_SO_SNDTIMEO, &sendTimeOut, sizeof( sendTimeOut ));
+        FreeRTOS_setsockopt(socket, 0, FREERTOS_SO_SNDTIMEO, &sendTimeOut, sizeof( sendTimeOut ));
 
         while (1) {
             memset(buf, 0x00, ipconfigTCP_MSS);
