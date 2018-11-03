d2fd0eab5f5e36d7911ed3b58959dbab3da37fcb
Alexander Wranovsky <alex@wranovsky.org>
2018-11-02 21:14:06 -0500

    Some fixes to get bootloader working

--- main.c
+++ main.c
@@ -8,8 +8,18 @@
 
 // tivaware includes
 #include "inc/hw_ints.h"
-#include "driverlib/sysctl.h"
+#include "inc/hw_types.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_nvic.h"
+#include "inc/hw_sysctl.h"
+#include "inc/hw_gpio.h"
 #include "driverlib/interrupt.h"
+#include "driverlib/flash.h"
+#include "driverlib/uart.h"
+#include "driverlib/gpio.h"
+#include "driverlib/rom.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/pin_map.h"
 
 // FreeRTOS includes
 #include "FreeRTOS.h"
@@ -28,9 +38,6 @@ extern void xPortSysTickHandler(void);
 // needed to read mac address (definition is in ethernet.c)
 extern bool eth_read_mac_addr( uint8_t *mac_buf );
 
-// place all your declarations here
-static void example_freertos_task(void *params);
-
 int main() {
     //////// NOTE: Do not move this section from main(). It breaks TCP ////////
     // set the system clock to the maximum value
@@ -38,6 +45,18 @@ int main() {
             SYSCTL_XTAL_25MHZ|SYSCTL_OSC_MAIN|SYSCTL_USE_PLL|SYSCTL_CFG_VCO_480,
             120000000
             );
+
+    // initialize buttons 0 and 1
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOJ);
+    while(!(SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOJ)));
+    GPIOPinTypeGPIOInput(GPIO_PORTJ_BASE, GPIO_PIN_0|GPIO_PIN_1);
+    HWREG(GPIO_PORTJ_BASE + GPIO_O_PUR) = GPIO_PIN_0|GPIO_PIN_1;
+
+    // initialize LED 0
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPION)) {}
+    GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_0);
+    GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, GPIO_PIN_0);
 
    //Wait for external peripherals to initialize
    {
@@ -52,9 +71,9 @@ int main() {
    IntRegister(FAULT_SYSTICK, xPortSysTickHandler);
 
     {
-        static const uint8_t Ip_Address[ipIP_ADDRESS_LENGTH_BYTES]      = {192,168,1,10};
+        static const uint8_t Ip_Address[ipIP_ADDRESS_LENGTH_BYTES]      = {192,168,0,10};
         static const uint8_t Net_Mask[ipIP_ADDRESS_LENGTH_BYTES]        = {255,255,255,0};
-        static const uint8_t Gateway_Address[ipIP_ADDRESS_LENGTH_BYTES] = {192,168,1,1};
+        static const uint8_t Gateway_Address[ipIP_ADDRESS_LENGTH_BYTES] = {192,168,0,1};
         static const uint8_t DNS_Address[ipIP_ADDRESS_LENGTH_BYTES]     = {8,8,8,8};
         static       uint8_t Mac_Address[6];
         eth_read_mac_addr(Mac_Address);
@@ -73,18 +92,8 @@ int main() {
     TaskHandle_t rip_task;
     uint8_t res = rip_init(&rip_task);
 
-    TaskHandle_t test_task;
-    xTaskCreate(example_freertos_task, "test_task", configMINIMAL_STACK_SIZE, NULL, 2, &test_task);
-
     // startup FreeRTOS, this function will never return on a system that's operating correctly
     vTaskStartScheduler();
 
     return 0;
-}
-
-static void example_freertos_task(void *params) {
-    // A FreeRTOS task should never return. If a task needs to be killed, use vTaskDelete().
-    while(1) {
-
-    }
 }
