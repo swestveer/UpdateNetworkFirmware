d215eea64dffd9b74544c7765b549081cf5e82aa
Alexander Wranovsky <alex@wranovsky.org>
2018-11-16 21:16:25 -0600

    Oops, deleted the bootloader task creation earlier, adding it back in.

--- main.c
+++ main.c
@@ -90,6 +90,8 @@ int main() {
     TaskHandle_t rip_task;
     uint8_t res = rip_init(&rip_task);
 
+    create_bootloader_test_task();
+
     // startup FreeRTOS, this function will never return on a system that's operating correctly
     vTaskStartScheduler();
 
